6da05e7f03472dc0c335522d2b64b9fd4d362da0
Obumuneme Nwabude <obumnwabude@gmail.com>
2020-04-01 15:21:36 +0100

    complete task for day 6

---
+++ day6/secret-map.js
@@ -0,0 +1,7 @@
+/** 
+ * Returns the squares of elements in an array 
+ * @author Obumuneme Nwabude <obumnwabude.com>
+ * @param {[number]} toSquare - the array of numbers to square its members
+ * @return {[number]} - the array of squared numbers
+ */
+const secretMap = toSquare => toSquare.map(value => Math.pow(value, 2));
