c7ef0aaaa6547687e3414d94df57032277c7464c
Obumuneme Nwabude <obumnwabude@gmail.com>
2020-04-01 13:47:11 +0100

    complete task of day 1

---
+++ day1/plus-litmus.js
@@ -0,0 +1,8 @@
+/** 
+ * Checks if the provided number argument is a negative or positive number
+ * @author Obumuneme Nwabude <obumnwabude.com>
+ * @param {number} toCheck - The number to check if its value is positive or negative
+ * @return {string} - 'Negative' if the number provided is a negative number 
+ * or 'Positive' if the provided number argument is a positive number
+ */
+const plusLitmus = toCheck => toCheck < 0 ? 'Negative' : 'Positive';
