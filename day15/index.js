af22c633e9cd70f6c9e21e79d40d9a64af09251f
Obumuneme Nwabude <obumnwabude@gmail.com>
2020-04-08 15:23:23 +0100

    setup server for sgpa calculator server of day 12

---
+++ day15/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs')
+const port = 3000 || process.env.PORT;
+
+app.use(bodyParser.urlencoded({ extended: false}));
+
+// for test
+app.use('/', express.static('public'));
+
+app.post('/createdata', (req, res) => {
+  try {
+    fs.writeFile('courses.json', JSON.stringify({courses: req.body.courses}), err => {
+      if (err) {
+        res.status(401).json({success:false});
+        return;
+      }
+      res.status(201).json({success:true});
+    });
+  } catch(error) {
+    res.status(401).end();
+  }
+});
+
+app.get('/getdata', (req, res) => {
+  try {
+    fs.readFile('courses.json', (err, courses) => {
+      if (err) {
+        res.status(200).json([]);
+        return;
+      }
+      res.status(200).json(JSON.parse(courses));
+    });
+  } catch(error) {
+    res.status(400).end();
+  }
+});
+
+module.exports = app.listen(port);
