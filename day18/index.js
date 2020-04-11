85fe8df74961b69b1ed4fbaa13370e7438be1c0e
Obumuneme Nwabude <obumnwabude@gmail.com>
2020-04-11 23:37:59 +0100

    setup login to login and authenticate mongodb user
    ensure valid email, username and password are passed during signup or login

--- day18/index.js
+++ day18/index.js
@@ -12,6 +12,14 @@ app.use(bodyParser.json());
 
 // handle signup
 app.post('/signup', (req, res) => {
+  // ensures that email, username and password are provided 
+  if (!(req.body.email)) 
+    return res.status(401).json({message: 'Please provide a valid email'});
+  else if (!(req.body.username))
+    return res.status(401).json({message: 'Please provide a valid username'});
+  else if (!(req.body.password))
+    return res.status(401).json({message: 'Please provide a password'});
+
   // hash the password from req.body 
   bcrypt.hash(req.body.password, 10)
     .then(hashed => {
@@ -26,7 +34,7 @@ app.post('/signup', (req, res) => {
       // save and return the user
       user.save()
         .then(() => res.status(201).json({
-          message: 'User successfully created',
+          message: 'User successfully created!',
           email: user.email,
           username: user.username 
         }))
@@ -37,7 +45,7 @@ app.post('/signup', (req, res) => {
             for (let [key, value] of Object.entries(err.errors)) {
               messages.push(`User with ${key}: ${value.value} exists already`);
             }
-            const returnMessage = messages.join('\n');
+            const returnMessage = messages.join('\n').concat('.');
             return res.status(401).json({message: returnMessage});
           }
           res.status(500).json(err);
@@ -46,46 +54,52 @@ app.post('/signup', (req, res) => {
 });
 
 // handle login
-app.post('/login', (req, res) => {
-  try {
-    // check if the users file for storage exists
-    if (fs.existsSync('users.json')) { 
-      // if so check if the user exists
-      fs.readFile('users.json', (err, users) => {
-        if (err) return res.status(401).json(err);
-        users = JSON.parse(`[${users}]`);
-        let user = users.find(one => one.email === req.body.email || one.username === req.body.username);
-        if (user) {
-          // if so compare passwords 
-          bcrypt.compare(req.body.password, user.password, (err, valid) => {
-            if (err) return res.status(401).json(err);
-            // if passwords match return succesful login message
-            if (valid) { 
-              // token signing
-              const token = jwt.sign({email: user.email}, 'RandoM_SECreT', {expiresIn: '5m'})
-              return res.status(201).json({
-                message: 'Login successful',
-                email: user.email,
-                username: user.username,
-                token: token
-              });
-            } else { 
-            // else return message of wrong password
-              return res.status(401).json({message: 'Wrong password, please login with correct password'});
-            }
-          });
-        } else {
-          // if not return message of user not found
-          res.status(401).json({message: 'User not found, please sign up'});
-        } 
+app.post('/login', async (req, res) => {
+  // lookup the user with email or username provided from the database
+  let user;
+  if (req.body.email || req.body.username) {
+    try {
+      if (req.body.email) {
+        user = await User.findOne({email: req.body.email});
+      } else if (req.body.username) {
+        user = await User.findOne({username: req.body.username});
+      }
+    } catch(error) {
+      return res.status(401).json(error);
+    }
+  } else {
+    // return if no email or username is passed to the body
+    res.status(401).json({message: 'Please provide a valid email or username to login.'});
+  }
+
+  // if a user was not found return the message that user was not found
+  if (!user) return res.status(401).json({
+    message: `User${req.body.email ? ' with email: ' + req.body.email : req.body.username ? 
+      ' with username: ' + req.body.username : '' } not found, please correct your details or sign up.`
+  });
+
+  // ensures that a password was passed
+  if (!(req.body.password))
+    return res.status(401).json({message: 'Please provide a password'});
+
+  // check if passwords match
+  bcrypt.compare(req.body.password, user.password)
+    .then(valid => {
+    // if passwords match return succesful login message with token
+    if (valid) { 
+      // token signing
+      const token = jwt.sign({email: user.email}, 'RandoM_SECreT', {expiresIn: '5m'})
+      return res.status(201).json({
+        message: 'Login successful!',
+        email: user.email,
+        username: user.username,
+        token: token
       });
     } else { 
-      // if not return a message to sign up
-      res.status(401).json({message: 'No user found!, sign up instead'});
+     // else return message of wrong password
+      return res.status(401).json({message: 'Wrong password, please login with correct password.'});
     }
-  } catch(error) {
-    res.status(500).json(error);
-  }
+  }).catch(err => res.status(500).json(err));
 });
 
 // handle getuser 
