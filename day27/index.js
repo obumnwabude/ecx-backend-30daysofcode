0ecfc89d226de9838aaf98df621f9132c2368691
Obumuneme Nwabude <obumnwabude@gmail.com>
2020-04-20 12:40:16 +0100

    add morgan middleware to wolverstore to log every request

--- day27/index.js
+++ day27/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 const port = process.env.PORT || 3000;
+
+// morgan middleware for logging
+app.use(morgan(':method :url :status :response-time ms'));
 
 app.get('/', (req, res) => {
   res.status(200).send('server working');
