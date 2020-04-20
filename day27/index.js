3b0a4db81dcb5a6f3b632e1d17d166622f866b8e
Obumuneme Nwabude <obumnwabude@gmail.com>
2020-04-20 12:46:39 +0100

    save logs to online mongodb database instance and retrieve them on /logs route

--- day27/index.js
+++ day27/index.js
@@ -1,13 +1,42 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Logger = require('./models/logger');
 const port = process.env.PORT || 3000;
 
+// connect to mongodb
+mongoose.connect('mongodb+srv://obum:24PFP3g7v6idbhcM@cluster0-kfvs4.gcp.mongodb.net/ecxbackend-wolverstore', 
+  {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => {
+    console.log('Successfully connected to MongoDB!');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB!');
+    console.error(error);
+  });
+
+// a logger to save logs to database
+const logStream = { 
+  write: line => {
+    const logger = new Logger({log: line});
+    logger.save().catch(err => console.log(err));
+  }
+};
+
 // morgan middleware for logging
-app.use(morgan(':method :url :status :response-time ms'));
+app.use(morgan(':method :url :status :response-time ms', {stream: logStream}));
 
 app.get('/', (req, res) => {
   res.status(200).send('server working');
 });
+
+app.get('/logs', (req, res) => {
+  Logger.find({}).then(logs => {
+    res.format({
+      'text/plain': () => res.status(200).send(logs.map(log => log.log).join(''))
+    });
+  }).catch(err => res.status(500).json(err));
+})
 
 module.exports = app.listen(port);
